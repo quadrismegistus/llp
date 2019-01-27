d4f796b6bd8b8a8f08292eebf671ed92f9303c13
quadrismegistus <ryan.heuser@gmail.com>
2019-01-26 19:38:03 -0800

    fixing things
    Former-commit-id: 9ba346693f5eeef143f17dc0d315d09bfaa96343

--- corpus/default/default.py
+++ corpus/default/default.py
@@ -8,7 +8,7 @@ class PlainTextCorpus(Corpus):
 
 	def __init__(self, PATH_TXT, PATH_METADATA=None, PATH_ROOT=None, name=None):
 		self.PATH_TXT=os.path.abspath(PATH_TXT)
-		self.PATH_METADATA=os.path.abspath(PATH_METADATA) if PATH_METADATA Else None
+		self.PATH_METADATA=os.path.abspath(PATH_METADATA) if PATH_METADATA else None
 		self.PATH_ROOT=os.path.split(PATH_TXT)[0]
 		super(PlainTextCorpus,self).__init__(
 						'PlainTextCorpus' if not name else name,
