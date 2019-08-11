1de012fd658a2832ffab52334c5be8675d638bab
quadrismegistus <ryan.heuser@gmail.com>
2019-08-10 21:14:50 -0700

    updates
    Former-commit-id: 794b37a47f4ce85a2bd53d1ed895be91a72c3bb0
    Former-commit-id: 55829be35bb3aae9e834e0ea8aed4047d7ea0b1c
    Former-commit-id: fd45a0f303cb303496628550c2d82da1af87c50e

---
+++ setup.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+import setuptools
+from setuptools import setup
+import os
+import sys
+_here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = [x.strip() for x in fh.read().split('\n') if x.strip()]
+
+setup(
+    name='llp',
+    version='0.1.1',
+    description=('Literary Language Processing (LLP): corpora, models, and tools for the digital humanities'),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Ryan Heuser',
+    author_email='heuser@stanford.edu',
+    url='https://github.com/quadrismegistus/llp',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    scripts=['bin/llp'],
+    include_package_data=True,
+    classifiers=[
+        #'Development Status :: 3 - Alpha',
+        #'Intended Audience :: Science/Research',
+        #'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3.6'
+    ],
+    )
